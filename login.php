aefa185a268cefe9fe65113f22c407f92b280d29
thanyarat <stangstang0508@gmail.com>
2022-05-03 23:29:43 +0700

    login

--- login.php
+++ login.php
@@ -6,17 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="./src/css/login.css" rel='stylesheet' />
+    <link type="text/css" href="./src/css/login.css" rel='stylesheet' />
     <title>Document</title>
 </head>
 
 <body>
-    <div class="row justify-content-center">
-        <div class='col-md-12 col-lg-10'>
-            <div class='wrap d-md-flex'>
-                <div class='img'></div>
-                <div></div>
+    
+    <div class="row min-vh-100">
+        <div class="BgLeft col-md-6">
+            <div class="wrap d-md-flex inLeft no-gutter">
+                <img src="./src/images/icon-head.png" class="icon" style="" />
+                <h3 class="head">ร้านวรเชษฐ์เกษตรภัณฑ์</h3>
             </div>
+        </div>
+        <div class="BgRight col-md-6">
+            <form>
+                <h1 class="text-login">Login</h1>
+                <label class="label" for="email">E-mail</label>
+                <input class="field input" type="email" name="email" require>
+                <label class="label" for="password">Password</label>
+                <input class="field input" type="password" name="password" require>
+                <div class="BTN">
+                    <input class="field  button" type="submit" value="Login" />
+                </div>
+
+            </form>
         </div>
     </div>
 </body>
