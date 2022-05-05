98353128b691b88735020fe05bf6866db629e028
suthasinee <suthasineelikely@gmail.com>
2022-05-05 20:01:21 +0700

    nav

---
+++ nav.php
@@ -0,0 +1,12 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A36627;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">xxxxxx</a>
+        <a herf="#"><img src=""/></a>
+        <div class="d-flex">
+            yyyyyyyyy
+        </div>
+    </div>
+    </div>
+</nav>
