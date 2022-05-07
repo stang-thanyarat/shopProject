bf80372677635b03c87f3bc5b7957542d7d251cf
thanyarat <stangstang0508@gmail.com>
2022-05-07 16:42:58 +0700

    employee

---
+++ employee.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./src/css/employee.css" />
+    <title>Document</title>
+</head>
+<?php include('nav.php'); ?>
+
+<body>
+    <form>
+        <div class="row main">
+            <div class="col-lg-8">
+                <h1>พนักงาน</h1>
+            </div>
+            <div class="col-lg-3">
+                <input type="text btn-c" placeholder="ชื่อ-นามสกุล">
+            </div>
+            <div class="col-lg-1">
+                <input class="submit btn-c" type="submit" value="เพิ่มพนักงาน" />
+            </div>
+        </div>
+        <table class="main ">
+            <tr>
+                <th>ชื่อ-นามสกุล</th>
+                <th>เลขบัตรประชาชน</th>
+                <th>สถานะการใช้งาน</th>
+                <th></th>
+            </tr>
+        </table>
+</body>
