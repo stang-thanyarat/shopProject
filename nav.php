166548e90c2f586d8ac59fd234fc1c5371ac1335
Padetsuek  Jangburee <padetsuek.2543@gmail.com>
2022-05-20 13:25:32 +0700

    update

--- nav.php
+++ nav.php
@@ -1,9 +1,7 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A36627;">
+<nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: #A36627;">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">xxxxxx</a>
-        <a herf="#"><img src=""/></a>
+        <a herf="#"><img src="" /></a>
         <div class="d-flex">
             yyyyyyyyy
         </div>
