70d4c07d651a475d5ceb6c401810e48a1d2b038a
suthasinee <suthasineelikely@gmail.com>
2022-07-14 23:07:26 +0700

    confirm

--- nav.php
+++ nav.php
@@ -1,9 +1,9 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: #A36627;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">xxxxxx</a>
+        <a class="navbar-brand" href="#">หน้าแรก</a>
         <a herf="#"><img src=""/></a>
         <div class="d-flex">
-            yyyyyyyyy
+        สมหญิง ถึงที่หมาย
         </div>
     </div>
     </div>
