743718cd60b8fcab7a67c62fed70fa10a0fca296
Padetsuek  Jangburee <padetsuek.2543@gmail.com>
2023-01-12 17:23:48 +0700

    12

--- database/Sales.php
+++ database/Sales.php
@@ -269,40 +269,41 @@ class Sales
 
     public function insert($data)
     {
-        $sql = "INSERT INTO sales_tb (product_id, sales_dt, payment_sl, employee_id, price, sales_amt, discount ,price_paid, import_files, note, stock_id)
-        VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        $sql = "SET FOREIGN_KEY_CHECKS=0";
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(1, $data['product_id'], PDO::PARAM_INT);
-        $stmt->bindParam(2, $data['sales_dt'], PDO::PARAM_STR);
-        $stmt->bindParam(3, $data['payment_sl'], PDO::PARAM_STR);
-        $stmt->bindParam(4, $data['employee_id'], PDO::PARAM_INT);
-        $stmt->bindParam(5, $data['price'], PDO::PARAM_STR);
-        $stmt->bindParam(6, $data['sales_amt'], PDO::PARAM_INT);
-        $stmt->bindParam(7, $data['discount'], PDO::PARAM_STR);
-        $stmt->bindParam(8, $data['price_paid'], PDO::PARAM_STR);
-        $stmt->bindParam(9, $data['import_files'], PDO::PARAM_STR);
-        $stmt->bindParam(10, $data['note'], PDO::PARAM_STR);
-        $stmt->bindParam(11, $data['stock_id'], PDO::PARAM_INT);
+        $stmt->execute();
+        $sql = "INSERT INTO sales_tb (payment_sl ,all_price ,all_quantity )
+        VALUES (?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(1, $data['payment_sl'], PDO::PARAM_STR);
+        $stmt->bindParam(2, $data['all_price'], PDO::PARAM_STR);
+        $stmt->bindParam(3, $data['all_quantity'], PDO::PARAM_STR);
+        //$stmt->bindParam(4, $data['employee_id'], PDO::PARAM_INT);
+        //$stmt->bindParam(5, $data['discount'], PDO::PARAM_STR);
+        //$stmt->bindParam(6, $data['sales_amt'], PDO::PARAM_INT);
+        //$stmt->bindParam(9, $data['import_files'], PDO::PARAM_STR);
+        //$stmt->bindParam(10, $data['note'], PDO::PARAM_STR);
+        //$stmt->bindParam(11, $data['stock_id'], PDO::PARAM_INT);
         $stmt->execute();
     }
 
     public function update($data)
     {
         $sql = "UPDATE sales_tb
-        SET product_id = ?, sales_dt = ?, payment_sl = ?, employee_id = ?, price = ?, sales_amt = ?, discount = ?, price_paid = ?, import_files = ?, note = ?, stock_id = ?
+        SET product_id = ?, sales_dt = ?, payment_sl = ?, employee_id = ?, discount = ?, sales_amt = ?, all_quantity = ?, all_price = ?, import_files = ?, note = ?, stock_id = ?
         WHERE sales_list_id = ?";
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(1, $data['product_id'], PDO::PARAM_INT);
-        $stmt->bindParam(2, $data['sales_dt'], PDO::PARAM_STR);
-        $stmt->bindParam(3, $data['payment_sl'], PDO::PARAM_STR);
-        $stmt->bindParam(4, $data['employee_id'], PDO::PARAM_INT);
-        $stmt->bindParam(5, $data['price'], PDO::PARAM_STR);
-        $stmt->bindParam(6, $data['sales_amt'], PDO::PARAM_INT);
-        $stmt->bindParam(7, $data['discount'], PDO::PARAM_STR);
-        $stmt->bindParam(8, $data['price_paid'], PDO::PARAM_STR);
-        $stmt->bindParam(9, $data['import_files'], PDO::PARAM_STR);
-        $stmt->bindParam(10, $data['note'], PDO::PARAM_STR);
-        $stmt->bindParam(11, $data['stock_id'], PDO::PARAM_INT);
+        //$stmt->bindParam(1, $data['product_id'], PDO::PARAM_INT);
+        //$stmt->bindParam(2, $data['sales_dt'], PDO::PARAM_STR);
+        //$stmt->bindParam(3, $data['payment_sl'], PDO::PARAM_STR);
+        //$stmt->bindParam(4, $data['employee_id'], PDO::PARAM_INT);
+        //$stmt->bindParam(5, $data['discount'], PDO::PARAM_STR);
+        //$stmt->bindParam(6, $data['sales_amt'], PDO::PARAM_INT);
+        $stmt->bindParam(7, $data['all_quantity'], PDO::PARAM_STR);
+        $stmt->bindParam(8, $data['all_price'], PDO::PARAM_STR);
+        //$stmt->bindParam(9, $data['import_files'], PDO::PARAM_STR);
+        //$stmt->bindParam(10, $data['note'], PDO::PARAM_STR);
+        //$stmt->bindParam(11, $data['stock_id'], PDO::PARAM_INT);
         $stmt->bindParam(12, $data['sales_list_id'], PDO::PARAM_INT);
         $stmt->execute();
     }
